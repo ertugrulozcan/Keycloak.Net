4c254ec8a6c60f9323275e4148a46c6a65279584
Ertuğrul Özcan <ertugrul.ozcan@demirorenteknoloji.com>
2020-03-06 14:18:43 +0300

    AspNetCore extensions

---
+++ Keycloak.Extensions.AspNetCore/IocExtensions.cs
@@ -0,0 +1,19 @@
+﻿using Keycloak.Boot;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace Keycloak.Extensions.AspNetCore
+{
+	public static class IocExtensions
+	{
+		public static void InitializeServices(this Bootstrapper bootstrapper, IServiceCollection services)
+		{
+			var serviceImplementations = bootstrapper.InitializeServices();
+			foreach (var serviceContract in serviceImplementations)
+			{
+				var serviceType = serviceContract.Key;
+				var serviceImpl = serviceContract.Value;
+				services.AddSingleton(serviceType, serviceImpl);
+			}
+		}
+	}
+}
